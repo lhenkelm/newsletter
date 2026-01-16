840b9e7d3240c37324c353746521ba4d927b11b3
lhenkelm <lars.henkelmann@gmail.com>
2026-01-16 12:44:59 +0100

    refactor: remove IngestConfig class and use direct environment variable access in config

--- src/newsletter/config.py
+++ src/newsletter/config.py
@@ -1,27 +1,11 @@
 """Configuration module for newsletter generator."""
 
 import os
-from dataclasses import dataclass
 
-
-@dataclass
-class IngestConfig:
-    """Configuration for data ingestion."""
-
-    cutoff_days: int
-    min_items: int
-    max_items: int
-    source_uri: str
-
-    @classmethod
-    def from_env(cls) -> "IngestConfig":
-        """Load configuration from environment variables with defaults."""
-        return cls(
-            cutoff_days=int(os.getenv("CUTOFF_DAYS", "7")),
-            min_items=int(os.getenv("MIN_ITEMS", "3")),
-            max_items=int(os.getenv("MAX_ITEMS", "30")),
-            source_uri=os.getenv(
-                "SOURCE_URI",
-                "hf://datasets/zenml/llmops-database/data/train-00000-of-00001.parquet",
-            ),
-        )
+CUTOFF_DAYS = int(os.getenv("CUTOFF_DAYS", "7"))
+MIN_ITEMS = int(os.getenv("MIN_ITEMS", "3"))
+MAX_ITEMS = int(os.getenv("MAX_ITEMS", "30"))
+SOURCE_URI = os.getenv(
+    "SOURCE_URI",
+    "hf://datasets/zenml/llmops-database/data/train-00000-of-00001.parquet",
+)
